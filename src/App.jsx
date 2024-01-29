cdffb656d443fe4ce89d87a5d2a54a3f891cfc3b
MOHAMMED SANAULLAH AVEZ <94492051+mohammedavez125@users.noreply.github.com>
2024-01-29 14:42:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+
+  return (
+      <div className="text-2xl font-bold underline border border-black">
+      {/* <div> */}
+      Hello world!
+    </div>
+  )
+}
+
+export default App
